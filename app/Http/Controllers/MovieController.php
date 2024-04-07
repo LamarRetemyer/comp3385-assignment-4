e68da69cda14f17a23d0568d6bfc01a08b9ec539
Lamar Retemyer <lamar.retemyer@gmail.com>
2024-04-06 20:46:10 -0400

    made chages to the MovieController and added the routes

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie; 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+
+class MovieController extends Controller
+{
+    public function index(){
+        //Query all the movies from the database. 
+        $movies = Movie::all();
+
+        //Prepare for the response data
+        $response=[
+            'message' => 'Movies successfully retrieved.',
+            'movies'=> $movies,
+        ];
+
+        //Return the data as JSON
+        return response()->json($response);
+
+    }
+
+    public function store(Request $request){
+        // Validate user input
+        $validator = Validator::make($request->all(),
+        [
+            'title' => 'required|string',
+            'description' => 'required|text',
+            'poster' => 'required|image|mimes:jpeg,png|max:2048', // Adjust allowed image types and max size
+        ]);
+
+        if ($validator->fails()) {
+            // Return validation error messages
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 422);
+        }   
+
+        // Save the uploaded poster image to the filesystem
+        $poster = $request->file('poster');
+        $posterPath = 'posters/' . $poster->hashName();
+        Storage::put($posterPath, file_get_contents($poster));
+
+        // Save movie information to the movies table
+        $movie = Movie::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'poster' => $posterPath,
+        ]);
+
+        $response = [
+            'message' => 'Movie created successfully',
+            'movie' => [
+                'title' => $movie->title,
+                'description' => $movie->description,
+                'poster' => $movie->poster,
+                'updated_at' => $movie->updated_at,
+                'created_at' => $movie->created_at,
+                'id' => $movie->id,
+            ],
+        ];
+
+        return response()->json($response);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// GET route for /api/v1/movies
+Route::get('/api/v1/movies', 'MovieController@index');
+
+// POST route for /api/v1/movies
+Route::post('/api/v1/movies', 'MovieController@store');
+
